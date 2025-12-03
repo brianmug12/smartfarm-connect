d4a22a4464c4aae0b90a1480b1f2cb244de02dcf
brianmug12 <mugishabrian525@gmail.com>
2025-12-03 19:52:00 +0200

    Create suppliers.tsx

---
+++ api/src/api/src/api/src/ussd/api/prisma/web/pages/web/pages/suppliers.tsx
@@ -0,0 +1,11 @@
+export default function Suppliers() {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-semibold">Suppliers</h2>
+      <ul>
+        <li>Gisozi Agro Supply</li>
+        <li>Kagarama Seeds</li>
+      </ul>
+    </div>
+  );
+}
