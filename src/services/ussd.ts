1f690547cd6fe74526270eba89dcac34879240d4
brianmug12 <mugishabrian525@gmail.com>
2025-12-04 15:39:51 +0200

    Add USSD service module

---
+++ src/services/ussd.ts
@@ -0,0 +1,6 @@
+// USSD module: Receive and process USSD requests
+
+export function handleUSSDRequest(payload: any) {
+  // TODO: Implement logic to parse and respond to USSD
+  return `USSD received: ${JSON.stringify(payload)}`;
+}
